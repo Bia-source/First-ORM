8561ba0d192c3b7b651c0665a7628db0d39ec868
Bia-source <bia_ferreirads@yahoo.com>
2024-08-05 19:02:22 -0300

    adicionando mensagens no json de resposta

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-// importando a representacao da tabela User
+// importando a representacao da tabela Message
 import { Message } from "../models/Message.model.js";
 
 export const createMessage = async (req, res) => {
@@ -11,7 +11,7 @@ export const createMessage = async (req, res) => {
         // da criacao da mensagem no banco de dados
         //              aguarde/ acessando tabela 'Message' / passando parametros de criacao
         const newMessage = await Message.create({user_sender_id, user_receiver_id, body});
-        return res.status(201).json({ newMessage });
+        return res.status(201).json({ newMessage, message: "mensagem criada com sucesso" });
     } catch (error) {
         return res.status(400).json({messageError: error.message})
     }
@@ -21,7 +21,7 @@ export const getAllMessages = async (req,res) => {
     try {
         // SELECT `id`, `username`, `statusOn`, `createdAt`, `updatedAt` FROM `Users` AS `User`;
         const messages = await Message.findAll();
-        return res.json({messages});
+        return res.status(200).json({messages});
     } catch (error) {
         return res.status(400).json({messageError})
     }
@@ -29,12 +29,13 @@ export const getAllMessages = async (req,res) => {
 
 export const deleteMessage = async (req,res) => {
     const { id } = req.params;
+    const message = await Message.findByPk(id);
     await Message.destroy({
         where: {
           id
         },
       });
-    return res.json({message: 'mensagem deletada com sucesso'})
+    return res.status(200).json({message, messageSuccess: 'mensagem deletada com sucesso'})
 }
 
 export const updatedMessage = async (req,res) => {
