efced34e6ddbc505ccd642de3d482f2f5aa89704
Bia-source <bia_ferreirad@yahoo.com>
2024-09-03 14:54:12 -0300

    retirando codigo de teste

--- src/server.js
+++ src/server.js
@@ -11,10 +11,6 @@ const port = 4000
 // google solicitar alguma rota do meu projeto
 // vai negado
 
-// teste git
-function teste(){
-  
-}
 // liberar todas as aplicacoes para acessar nossas rotas
 app.use(cors());
 
